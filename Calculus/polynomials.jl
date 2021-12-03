5bf0eeb1aa773914d7c0f42fec90be86f03add3c
TJ Davis <thomas.davis@mavs.uta.edu>
2021-12-03 15:57:04 +0800

    Update polynomials.jl
    debugged a little, added polynomials to test on

--- Calculus/polynomials.jl
+++ Calculus/polynomials.jl
@@ -1,4 +1,5 @@
 export poly, hasRationalRoots
+
 struct poly
   n::UInt64
   coeffs::Vector{Int64}
@@ -6,14 +7,21 @@ struct poly
 end
 
 #evaluates the polynomial at x
+#currently bugged - try horner's method instead
 function eval(p::poly,x)
-  sum = poly.coeffs[1]
+  sum = p.coeffs[1]
   for i in 2:(p.n)+1
     sum += p.coeffs[i]*(x)^(i+1)
   end
   sum
 end
 
+#=currently misses roots. I suspect eval isn't working properly
+test cases:
+	2x^3+x-1 -> no rational roots
+	x^3-7x+6 -> rational roots at x=1,2,-3
+	
+=#
 function hasRationalRoots(p::poly)
   roots = []
   f0 = factor(p.coeffs[1])
@@ -22,16 +30,16 @@ function hasRationalRoots(p::poly)
     for j in 1:length(fn)
       testRoot1 = f0[i]/fn[j]
       testRoot2 = -f0[i]/fn[j]
-      if eval(p,testR
-        oot1)==0
+
+      if eval(p,testRoot1)==0
         push!(roots,testRoot1)
       end
       if eval(p,testRoot2)==0
-        push!(roots,testRoot2)
+		push!(roots,testRoot2)
       end
     end
   end
-  if length(roots)>0
+  if length(roots)==0
     return "No rational roots"
   else
     return roots
@@ -50,6 +58,5 @@ function factor(n::Int)
   end
  push!(f,n)
 end
-
 
 
